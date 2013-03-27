949b07f8e208c3eb8638aaaee42861182c7889f7
ydx <x6doooo@gmail.com>
2013-03-27 08:46:21 +0800

    主绘制对象

---
+++ src/sivona.canvas.js
@@ -0,0 +1,26 @@
+var Canvas = function(container_id, w, h){
+  var self = this,
+    ct,
+    cn;
+  if(isUndefined(container_id) || !isString(container_id)){
+    throw new Error(ERROR_STRING.container);
+  }
+  if(isUndefined(w)){
+    w = window.screen.width;
+  }
+  if(isUndefined(h)){
+    h = window.screen.height;
+  }
+  ct = document.getElementById(container_id);
+  if(ct === null){
+    throw new Error(ERROR_STRING.container);
+  }
+  cn = document.createElement('canvas');
+  cn.width = w;
+  cn.height = h;
+  ct.appendChild(cn);
+  self.container = ct;
+  self.canvasNode = cn;
+  self.canvasContext = cn.getContext('2d');
+};
+
