b6a85c3595d4cf9d58510721d5bd5d45c7427980
ydx <x6doooo@gmail.com>
2013-03-27 10:32:37 +0800

    静态和方法

---
+++ src/sivona.const.js
@@ -0,0 +1,17 @@
+var PI = Math.PI,
+  abs = Math.abs,
+  random = Math.random,
+  mmax = Math.max,
+  mmin = Math.min;
+
+function deg2rad(d){
+  return d * PI / 180;
+}
+
+function rad2deg(r){
+  return r * 180 / PI;
+}
+
+ERROR_STR = {
+  'container': 'Container is undefined!'
+};
