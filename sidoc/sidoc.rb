5d53aab0639e92d4788db921d5ad92011ecefa66
ydx <x6doooo@gmail.com>
2013-03-28 08:13:02 +0800

    创建打包工具
    针对自己习惯的注释方法开发一个文档生成脚本 需要有打包功能

---
+++ sidoc/sidoc.rb
@@ -0,0 +1,43 @@
+if ARGV.empty?
+  src_path = '../src/sivona.'
+  tar_path = './si-doc.md'
+end
+
+config = [
+    'prefix',
+    'util.js',
+    'const.js',
+    'init.js',
+    'canvas.js',
+    'exports.js',
+    'suffix'
+]
+
+config = config.map{|v| src_path + v}
+
+files = []
+config.each do |path|
+  File.open(path, 'r') do |lines|
+    lines.each do |line|
+      files.push line
+    end
+  end
+end
+
+files = files.join
+
+heads = {
+    "@Title" => "#;",
+    "@Link" => "<;>",
+    "@Copyright" => "",
+    "@License" => ""
+}
+tags = [
+    "@Params",
+    "@Info",
+    "@Type",
+    "@Usage",
+    "@Return"
+]
+
+puts files.scan(/\/\*![^*]*\*+(?:[^*\/][^*]*\*+)*\//)
