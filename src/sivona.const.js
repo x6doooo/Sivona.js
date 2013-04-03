894886cfd53f444cb36799e64c6401329e0ac899
ydx <x6doooo@gmail.com>
2013-04-03 13:57:26 +0800

    修正颜色值转换

--- src/sivona.const.js
+++ src/sivona.const.js
@@ -21,7 +21,25 @@ var PI = Math.PI,
   },
   sin = Math.sin,
   cos = Math.cos,
-  tan = Math.tan;
+  tan = Math.tan,
+  webColors = {
+    aqua: '#0ff',
+    black: '#000',
+    blue: '#00f',
+    fuchsia: '#f0f',
+    gray: '#808080',
+    green: '#008000',
+    lime: '#0f0',
+    maroon: '#800000',
+    navy: '#000080',
+    olive: '#808000',
+    purple: '#800080',
+    red: '#f00',
+    silver: '#c0c0c0',
+    teal: '#008080',
+    white: '#fff',
+    yellow: '#ff0'
+  };
 
 (function() {
   var lastTime = 0;
@@ -58,13 +76,19 @@ function rad2deg(r){
 
 // hex2num('#369') => [51, 102, 153]
 function hex2num(v){
-  var arr = v.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, function(m, r, g, b) {
-    return [parseInt(r + r, 16), parseInt(g + g, 16), parseInt(b + b, 16)];
+  if(v.search('#') == -1) v = webColors[v];
+  if(v.length == 4){
+    v = v.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+  }
+  v = v.replace(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i, function(m, r, g, b){
+    return [parseInt(r, 16), parseInt(g, 16), parseInt(b, 16)];
   }).split(',');
-  arr.forEach(function(v, i, a){
-    a[i] = to_i(v);
+  v.forEach(function(val, i, a){
+    a[i] = to_i(val);
   });
-  return arr;
+  return v;
 }
 
 // rgb2hex(51,102,153); => '#369'
