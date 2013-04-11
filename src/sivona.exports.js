cf3ea3eeebbed3a495a715e4a70e37aad39da97e
ydx <x6doooo@gmail.com>
2013-04-11 15:11:48 +0800

    SI.parsePath方法
    将Cpath类的parse方法交给SI对象输出

--- src/sivona.exports.js
+++ src/sivona.exports.js
@@ -2,5 +2,18 @@ SI.Paper = Paper;
 SI.version = Version;
 SI.animationController = sanimator;
 SI.getEventPosition = getEventPosition;
+/*!
+
+  @Name: SI.parsePath(d)
+  @Info: 解析SVG路径描述，转为Canvas绘制用的命令和点的数组
+  @Params:
+  - d {string} svg路径字符串
+  @Return:
+  - {array}
+  @Usage:
+  arr = SI.parsePath('M10,10L10,10C-10-10...');
+  //arr = [{type:'moveTo',points:[10,10]}, {type:'lineTo',points:[10,10]}...]
+
+*/
 SI.parsePath = Cpath.parse;
 window.SIVONA = window.SI = SI;
