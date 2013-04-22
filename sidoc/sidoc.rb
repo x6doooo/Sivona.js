c4ee916366567d5c1d29d24eae0769ed6e3e6517
ydx <x6doooo@gmail.com>
2013-04-22 15:05:40 +0800

    更新输出工具
    初步完成api部分，输出的api文档为markdown文件。

--- sidoc/sidoc.rb
+++ sidoc/sidoc.rb
@@ -1,20 +1,22 @@
 class Src_loader
   attr_accessor :src_path, :file_name_list, :list
+
   def format_path
     @list = @file_name_list.map{|v| @src_path + v}
   end
+
   def load
+    files = []
     puts '--- load start'
     if @list.nil? 
       format_path
     end
-    files = []
-    @list.each do |path|
-      File.open(path, 'r') do |lines|
-        lines.each { |line| files.push line }
-      end
+    @list.each{ |path|
+      file = open(path)
+      files << file.read
+      file.close
       puts "--- #{path} loaded"
-    end
+    }
     puts '--- load end'
     files.join
   end
@@ -23,29 +25,37 @@ end
 class File_maker
   attr_accessor :content, :api_doc
   def format_api_doc
+    isUsage = false
     @api_doc = @content.scan(/\/\*![^Private][^*]*\*+(?:[^*\/][^*]*\*+)*\//)
-    @api_doc = @api_doc.map{|line|
+    @api_doc.map!{|line|
       line = line.gsub(/^\s+|^\t+/, "")
-      line = line.gsub(/\n/, "\n\n")
-      line = line.gsub(/\/\*\!/, "===")
-      line = line.gsub(/\*\//, "\n")
-      line = line.gsub(/@Title:\s*/, "\#")
-      line = line.gsub(/(@Link\:\s*)(.+$)/, '\\1<\\2>')
+        .gsub(/(^[@-])/, "\n\\1")
+        .gsub(/\/\*\!/, "===")
+        .gsub(/\*\//, "\n")
+        .gsub(/@Title:\s*/, "#")
+        .gsub(/(@Link\:\s*)(.+$)/, '\\1<\\2>')
+        .gsub(/@Name:\s*/, "###")
+        .gsub(/(@Usage:.*\n)/, "\\1\n```\n")
+        .gsub(/(^\|.*\n*)(^[^\|])/, "\\1```\\2")
+        .gsub(/^\|/, '')
     }
     @api_doc = @api_doc.join
   end
+
   def export_api_doc(f)
     if @api_doc.nil?
       format_api_doc
     end
     export(f, @api_doc)
   end
+
   def export(f, c)
     File.open(f, 'w') do |file|
       file.write(c)
     end
     puts "--- export #{f} done"
   end
+
   def export_whole_file(f)
     export f, @content
   end
@@ -66,10 +76,9 @@ loader.file_name_list = %W(
     suffix
 )
 
-loader.format_path
-
 maker = File_maker.new
 maker.content = loader.load
+
 maker.export_whole_file '../src/sivona.js'
 maker.export_api_doc './api.md'
 
